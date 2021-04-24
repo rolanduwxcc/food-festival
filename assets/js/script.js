bd382455ff56bb0f08ecdb29612bcccb085ee2bf
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-04-24 18:08:31 -0500

    pwa added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@ const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
 
 $(document).ready(function() {
-
-
   // First image is hard coded in index.html
   const carouselSlides = [
     {
@@ -15,7 +13,7 @@ $(document).ready(function() {
       btnUrl: "schedule.html"
     },
     {
-      title: "Our food is seriously the bomb!",
+      title: "Our food is seriously the F bomb!",
       subtitle: "What are you waiting for?",
       img: img2,
       btnText: "Purchase Tickets",
